ea9ef513c2f4168034ca83889c7fd3d87b48097d
ashupawlos <ashenafipawlos939@gmail.com>
2023-12-19 12:31:09 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
- * main - prints _putchar, fllowd by a new linees
- * Return: Alwys 0 (Success)
+ * main - Entry point
+ * Return:0
  */
+
 int main(void)
 {
-	char abe[] = "_putchar";
-
-	int c;
-
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(abe[c]);
-	}
-	_putchar('\n');
-	return (0);
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
